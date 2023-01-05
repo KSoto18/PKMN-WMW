c80edf6d1775fadf938b0e6bf704de6083e834bb
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-01-05 09:11:40 -0500

    added header component to Apps.js

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => { 
+
+
+    return (
+
+        <div> Whats My Weakness?</div>
+
+    )
+       
+
+
+}
+
+
+
+
+
+export default Header;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles.css';
+
+const Navbar = () => { 
+
+     return (
+
+         <div> Navbar </div>
+
+     )
+        
+
+
+}
+
+
+
+
+
+export default Navbar;
