1bccb9968499c5776958e2202d10717bef6502b5
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-01-05 17:33:39 -0500

    updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ export default function ABOUT() {
 
             <div>
                 <h1>About</h1>
-                <p>Whats My Weakness is a simple application that helps you find quickly the weakness to a certain type selected by the user.</p>
+                <p>Whats My Weakness is a simple application that helps Trainers quickly find the WEAKNESS to a certain TYPE selected by the user.</p>
                 <p>WMW is only compatible with Pokemon Scarlet & Violet at this time.</p>
             </div>
 
@@ -17,6 +17,14 @@ export default function ABOUT() {
                 <li>The WEAKNESSES for the selected TYPE populate underneath your selection.</li>
                 <li>Go back to you game and pick the best POKEMON to counter your fight acording to your findings!</li>
              </ul>
+          </div>
+
+          <div>
+            <h3>More Information</h3>
+            <p>This app is not affiliated, endorsed or supported by Nintendo or The Pokemon Company.
+            <p>Its sole purpose is to aid Trainers in need of information during gameplay.</p>
+            <p>No copyright infringement intended.</p>
+            </p>
           </div>
 
           <div>
