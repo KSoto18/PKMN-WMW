a0dcbd92e18001a4fd71d5b684161d9fcc5ae1c0
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-01-09 18:41:00 -0500

    styling contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,11 +1,19 @@
+// Styling
+import './styles.css';
+
+
+// Icons
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 export default function Contact() {
     return (
 
-        <div>
+        <div className='contactBox'>
 
-            <h1>CONTACT</h1>
-            <p>GITHUB</p>
-            <p><a href='mailto:dev.ksoto@gmail.com'>EMAIL</a></p>
+            <h1 className= 'title'>CONTACT ME</h1>
+            <p><a href='https://github.com/KSoto18'><GitHubIcon /> GITHUB</a></p>
+            <p><a href='mailto:dev.ksoto@gmail.com'><EmailIcon />  EMAIL</a></p>
             
         </div>
 
