355942e3c029fb927d0af7436e66456f511f56d7
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-01-09 14:52:37 -0500

    styling navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,15 @@
+// Main Imports
 import React from 'react';
 
+// Styling
 import './styles.css';
+
+// Icons
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import InfoIcon from '@mui/icons-material/Info';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+
+
 
 const Navbar = ({ showAbout, showBody, showContact, active }) => {
 
@@ -9,9 +18,9 @@ const Navbar = ({ showAbout, showBody, showContact, active }) => {
         <div className='navbar'>
         
            <nav className='nav'>
-            <h1 onClick={showBody} className={active === 1 ? 'links active' : 'links'}>HOME</h1>
-            <h1 onClick={showAbout} className={active === 1 ? 'links active' : 'links'}>ABOUT</h1>
-            <h1 onClick={showContact} className={active === 1 ? 'links active' : 'links'}>CONTACT</h1>
+            <h1 onClick={showBody} className={active === 1 ? 'links active' : 'links'}><CatchingPokemonIcon/> HOME</h1>
+            <h1 onClick={showAbout} className={active === 1 ? 'links active' : 'links'}><InfoIcon /> ABOUT</h1>
+            <h1 onClick={showContact} className={active === 1 ? 'links active' : 'links'}><InsertEmoticonIcon /> CONTACT</h1>
            </nav>
            
 
